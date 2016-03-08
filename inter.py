dbe2710766453db05e4671f86230c04242ef846a
Rohan Bojja <rohan.bojja@gmail.com>
2016-03-08 17:27:12 +0000

    Added the servo stuff.

--- inter.py
+++ inter.py
@@ -1,3 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
+pwm=GPIO.PWM(11,50)
+pwm2=GPIO.PWM(15,50)
+pwm.start(5)
+pwm2.start(5)
+duty1=2.5
+
+duty2=12.5
+
+ck=0
+while ck<=5:
+    pwm.ChangeDutyCycle(duty1)
+    pwm2.ChangeDutyCycle(duty1)
+    time.sleep(0.8)
+    pwm.ChangeDutyCycle(duty2)
+    pwm2.ChangeDutyCycle(duty2)
+    time.sleep(0.8)
+    ck=ck+1
+time.sleep(1)
 #Interprets the moves in "seqin.txt" in this directory.
 f = open("seqin.txt","r")
 strin=f.read()#The string
@@ -9,3 +33,4 @@ print str2
 while(len(str2)):
 	print str2[len(str2)-1]#REPLACE THIS WITH THE ACTUAL THING, Hardware Interaction
 	str2.pop()
+GPIO.cleanup()
