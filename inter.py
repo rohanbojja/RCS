8bc8ed3e86a3d4350a9895b0104843c5047778bd
Rohan Bojja <rohan.bojja@gmail.com>
2016-03-09 09:00:52 +0000

    Updated  stuff

--- inter.py
+++ inter.py
@@ -1,23 +1,26 @@
 import RPi.GPIO as GPIO
 import time
-
+#11,13 and 15 are Servos
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11,GPIO.OUT)
-GPIO.setup(15,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)#Servo 1
+GPIO.setup(13,GPIO.OUT)#Servo 2
+GPIO.setup(15,GPIO.OUT)#Servo 3
 pwm=GPIO.PWM(11,50)
-pwm2=GPIO.PWM(15,50)
-pwm.start(5)
-pwm2.start(5)
+pwm2=GPIO.PWM(13,50)
+pwm3=GPIO.PWM(15,50)
+pwm.start(2.5)
+pwm2.start(2.5)
+pwm3.start(2.5)
 duty1=2.5
-
-duty2=12.5
-
+duty2=7.5
 ck=0
 while ck<=5:
     pwm.ChangeDutyCycle(duty1)
     pwm2.ChangeDutyCycle(duty1)
+    pwm3.ChangeDutyCycle(duty1)
     time.sleep(0.8)
     pwm.ChangeDutyCycle(duty2)
+    pwm2.ChangeDutyCycle(duty2)
     pwm2.ChangeDutyCycle(duty2)
     time.sleep(0.8)
     ck=ck+1
